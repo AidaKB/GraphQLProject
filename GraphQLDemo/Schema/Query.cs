c12b1a0252fea36b561e011f2e55d7ab4e48131a
Aida Beheshti <aida.beheshti81@gmail.com>
2025-06-29 10:13:33 +0330

    initial commit

---
+++ GraphQLDemo/Schema/Query.cs
@@ -0,0 +1,8 @@
+﻿namespace GraphQLDemo.Schema
+{
+    public class Query
+    {
+        [GraphQLDeprecated("this query is deprecated")]
+        public string Instruction => "Use the GraphQL endpoint to query data. For example, you can query for a list of users or a specific user by ID.";
+    }
+}
